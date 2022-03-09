e66ec52586bb90ed9c98042e325d1082766e7181
code2828 <stevenhaocheng@163.com>
2022-03-09 11:38:00 +0000

    Finished Line #1 of Beijing and a few changes

---
+++ whatever.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include"json.hpp"
+using namespace std;
+
+class Lines
+{
+    //1,2,4,5,6,7,8,9,10,11,13,14,15,16,17,19,s1,cp,sdjc,yz,dxjc,fs,yf, 23 in all
+};
+
+class Map
+{
+    int number_of_lines;
+    int number_of_stations;
+    vector<Lines> lines;
+    
+};
+
+vector<Map> maps;
+
+int main()
+{
+    return 0;
+}
